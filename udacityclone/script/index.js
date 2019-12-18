16e8d147e16a9e7fa89fa0e9858724959445538a
SivaSundaraNarayanan <sivasundaranarayanan@gmail.com>
2019-12-18 17:47:22 +0530

    Udacity clone

---
+++ udacityclone/script/index.js
@@ -0,0 +1,29 @@
+window.onscroll = () => {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset >= 150) {
+        document.getElementById("wrapper").classList.add("sticky");
+    } else {
+        document.getElementById("wrapper").classList.remove("sticky");
+    }
+}
+
+
+
+var intercom = () => {
+    var intercomms = document.getElementsByClassName("intercom-button");
+    var comms;
+    console.log(intercomms);
+    for (let ind = 0; ind < intercomms.length; ind++)
+        intercomms[ind].classList.toggle("show");
+};
+
+var toggle = () => {
+    let div_width = document.getElementById("mov-sidenav").style.width;
+    if (div_width == "250px") {
+        document.getElementById("mov-sidenav").style.width = "0";
+        document.getElementsByTagName("body")[0].style.marginLeft = "0";
+    } else {
+        document.getElementById("mov-sidenav").style.width = "250px";
+        document.getElementsByTagName("body")[0].style.marginLeft = "250px";
+    }
+}
